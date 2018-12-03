e9b7083f3275bb4da7b4f57553a588f2caf437dd
Gustavo Muniz do Carmo <gustavo@codeyourinfra.today>
2018-12-03 15:01:06 +0000

    upgrade to bionic

--- jenkins/bake.sh
+++ jenkins/bake.sh
@@ -10,11 +10,11 @@ echo "Provisioning the server with the latest Oracle Java 8 and Jenkins"
 vagrant provision
 
 echo "Baking the jenkins server"
-vagrant package --output jenkins.box
+vagrant package --output jenkins-$(date '+%Y%m%d.0.0').box
 
 echo "Removing the jenkins server"
 vagrant destroy -f
 
 echo "Cleaning up everything and exiting"
-rm -rf .vagrant/ ubuntu-xenial-16.04-cloudimg-console.log roles/
+rm -rf .vagrant/ ubuntu-*-cloudimg-console.log roles/
 exit 0
