10646fc2c96beb43880d4adb366a1ee3a5508b25
Gustavo Muniz do Carmo <gustavo@codeyourinfra.today>
2018-12-03 15:21:22 +0000

    rm prior box file

--- jenkins/bake.sh
+++ jenkins/bake.sh
@@ -10,7 +10,9 @@ echo "Provisioning the server with the latest Oracle Java 8 and Jenkins"
 vagrant provision
 
 echo "Baking the jenkins server"
-vagrant package --output jenkins-$(date '+%Y%m%d.0.0').box
+BOX_VERSION=$(date '+%Y%m%d.0.0')
+BOX_FILE=jenkins-$BOX_VERSION.box
+rm -f $BOX_FILE && vagrant package --output $BOX_FILE
 
 echo "Removing the jenkins server"
 vagrant destroy -f
