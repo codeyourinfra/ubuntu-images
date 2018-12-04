c58e317638a9eff562b4f29f3ba0b284e681f698
Gustavo Muniz do Carmo <gustavo@codeyourinfra.today>
2018-12-04 11:20:58 +0000

    publish new box in vagrant cloud

--- jenkins/bake.sh
+++ jenkins/bake.sh
@@ -16,13 +16,13 @@ vagrant provision
 echo "Baking the jenkins server"
 rm -f $BOX_FILE && vagrant package --output $BOX_FILE
 
-echo "Creating the new box version in Vagrant Cloud"
+echo "Publish the new box version in Vagrant Cloud"
 BASE_BOX_VERSION=$(grep -o "'[a-zA-Z0-9]*/[a-zA-Z0-9]*' (v[0-9.]*)" -m 1 box-update-output.txt)
 UBUNTU_VERSION=$(cat ubuntu-version.txt)
 JAVA_VERSION=$(cat java-version.txt)
 JENKINS_VERSION=$(cat jenkins-version.txt)
 VERSION_DESCRIPTION="Built from $BASE_BOX_VERSION - $UBUNTU_VERSION - and provisioned with Oracle JDK $JAVA_VERSION and Jenkins $JENKINS_VERSION."
-vagrant cloud version create -d "$VERSION_DESCRIPTION" codeyourinfra/jenkins $BOX_VERSION
+vagrant cloud publish codeyourinfra/jenkins $BOX_VERSION virtualbox $BOX_FILE --version-description "$VERSION_DESCRIPTION" --force --release
 
 echo "Removing the jenkins server"
 vagrant destroy -f
