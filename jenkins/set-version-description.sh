aa64da4393da4a73bc1b372b8b1a5a10aa477025
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2019-05-10 20:31:11 +0100

    replace oracle jdk with openjdk

--- jenkins/set-version-description.sh
+++ jenkins/set-version-description.sh
@@ -2,4 +2,4 @@
 
 JAVA_VERSION=$(cat java-version.txt)
 JENKINS_VERSION=$(cat jenkins-version.txt)
-VERSION_DESCRIPTION="Built from $BASE_BOX_VERSION - $UBUNTU_VERSION - and provisioned with Oracle JDK $JAVA_VERSION and Jenkins $JENKINS_VERSION."
+VERSION_DESCRIPTION="Built from $BASE_BOX_VERSION - $UBUNTU_VERSION - and provisioned with OpenJDK $JAVA_VERSION and Jenkins $JENKINS_VERSION."
